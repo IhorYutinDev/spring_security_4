af09d2c01febbcf903fc2a7426a327ffea6b4d6c
ihor <ihor.yutin.dev@gmail.com>
2024-12-26 11:41:17 +0200

    T60001-2

--- src/main/java/ua/ihor/SpringBootSecurity4/config/New5001.java
+++ src/main/java/ua/ihor/SpringBootSecurity4/config/New5001.java
@@ -4,6 +4,6 @@ public class New5001 {
     public static void main(String[] args) {
         System.out.println("hello world" +
                 "" +
-                "1");
+                "2");
     }
 }
